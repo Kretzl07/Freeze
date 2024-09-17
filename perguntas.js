e20491464fede58cc803a64a04fc3e1aa24e6d2a
LadyBug <105938774+Kretzl07@users.noreply.github.com>
2024-09-17 08:25:07 -0300

    ..

---
+++ perguntas.js
@@ -0,0 +1,49 @@
+criaCartão(
+    'biologia',
+    'Qual é a função das mitocôndrias nas células?',
+    'As mitocôndrias são responsáveis pela produção de energia na forma de ATP, através da respiração celular.')
+
+criaCartão(
+    'biologia',
+    'O que é fotossíntese e qual a sua importância para os seres vivos?',
+    'A fotossíntese é o processo pelo qual plantas, algas e algumas bactérias convertem luz solar em energia química, essencial para a produção de oxigênio e alimentos.')
+
+criaCartão(
+    'biologia',
+    'Qual a diferença entre célula procarionte e eucarionte?',
+    'Células procariontes não têm núcleo organizado, enquanto as células eucariontes possuem núcleo e organelas membranosas.')
+
+criaCartão(
+    'biologia',
+    'Qual é a função do DNA no organismo?',
+    'O DNA contém as instruções genéticas necessárias para o desenvolvimento e funcionamento de todos os seres vivos, e é responsável pela hereditariedade.')
+
+criaCartão(
+    'biologia',
+    'O que são enzimas e qual é o seu papel nas reações químicas do corpo?',
+    ' Enzimas são proteínas que catalisam reações químicas, acelerando-as sem serem consumidas no processo.')
+
+criaCartão(
+    'biologia',
+    'Explique a teoria da seleção natural de Charles Darwin.',
+    'A seleção natural é o processo pelo qual organismos mais adaptados ao ambiente têm maior chance de sobreviver e se reproduzir, transmitindo suas características.')
+
+criaCartão(
+    'biologia',
+    'O que são ecossistemas e como os organismos interagem dentro deles?',
+    'Ecossistemas são comunidades de organismos que interagem entre si e com o ambiente físico. As interações incluem competição, predação, simbiose, entre outro')
+
+criaCartão(
+    'biologia',
+    'O que diferencia os animais vertebrados dos invertebrados?',
+    ' Vertebrados possuem coluna vertebral e um esqueleto interno, enquanto os invertebrados não têm coluna vertebral.')
+
+criaCartão(
+    'biologia',
+    'O que é o ciclo do nitrogênio e qual sua importância para os seres vivos?',
+    ' O ciclo do nitrogênio é o processo de conversão do nitrogênio atmosférico em compostos utilizáveis pelas plantas. É crucial para a produção de proteínas e ácidos nucleicos.')
+
+criaCartão(
+    'biologia',
+    'Como o sistema imunológico atua na defesa do corpo contra doenças?',
+    'O sistema imunológico detecta e combate patógenos como bactérias e vírus, utilizando células de defesa, anticorpos e mecanismos de memória para proteger o corpo.')
