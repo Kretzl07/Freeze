940025beea63080fe811cc7b524cfd4012331532
LadyBug <105938774+Kretzl07@users.noreply.github.com>
2024-09-26 08:10:49 -0300

    hg

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function criaCartao(categoria, pergunta, resposta){
 let container = document.getElementById('container');
 let Cartao = document.createElement('article');
 Cartao.className = 'Cartao';
-Cartao.innerHtml = `
+Cartao.innerHTML = `
 <div class = "Cartao-conteudo">
 <h3> ${categoria} </h3>
 <div class = "Cartao-pergunta">
