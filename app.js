ff020959d2deed6553ca2290a1535080aa705dc7
LadyBug <105938774+Kretzl07@users.noreply.github.com>
2024-09-26 08:04:04 -0300

    vv

---
+++ app.js
@@ -0,0 +1,16 @@
+function criaCartão(categoria, perguntas, resposta){}
+let container = document.getElementById('container');
+let Cartão = document.createElement('article');
+Cartão.className = 'Cartão';
+Cartão.imerHtml = `
+<div class = "Cartão-conteudo">
+<h3> ${categoria} </h3>
+<div class = "Cartão - pergunta">
+<p> ${pergunta} </p>
+</div>
+<div class = "Cartão - resposta">
+<p> ${resposta} </p>
+</div>
+</div>
+`
+container.appendChild(Cartão);
